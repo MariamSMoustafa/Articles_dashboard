9e7d939ee6cd3b06ce64948041149017ff1553c1
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-04-28 00:04:20 +0200

    test login

---
+++ controllers/login.php
@@ -0,0 +1,61 @@
+<?php 
+require_once("../vendor/autoload.php");
+
+try {
+    session_start();
+    $conn= mysqli_connect(__HOST__, __USER__, __PASS__, __DB__);
+    if(!$conn) {
+        echo 'Connection failed';
+        throw new Exception("Connection failed");
+    } else {
+        if(isset($_POST['uname']) && isset($_POST['password'])) {
+            $uname= ($_POST['uname']);
+            $pass=($_POST['password']);
+            if(empty($uname)) {
+                header("Location: ../?error=Username is required");
+                throw new Exception("User didn't write username");
+            } elseif(empty($pass)) {
+                header("Location: ../?error=Password is required");
+                throw new Exception("User didn't write password");
+            } else {
+                $users_db= new MySQLHandler('users');
+                $sql= "SELECT * FROM users WHERE name= '$uname' AND password='$pass'";
+                $user=$users_db->get_results($sql);
+                if(!isset($user[0])){
+                    header("Location: ../?error=Incorrect username or password");
+                    throw new Exception("No such user in database");
+                }else{
+                    $id = $user[0]['id'];
+                    // $last_login=$user[0]['last_login'];
+                    // date_default_timezone_set('Africa/Cairo');
+                // $values = [
+                //     "last_login" => date('Y-m-d h:i:sa',time())
+                //   ];
+                //  $users_db->update($values,$id); 
+                    $groups_db= new MySQLHandler('groups');
+                $group=$groups_db->get_record_by_id($user[0]['group_id']);
+                    // $_SESSION['user_name']=$user[0]['user_name'];
+                    $_SESSION['name']=$user[0]['name'];
+                    $_SESSION['password']=$user[0]['password'];
+                    $_SESSION['email']=$user[0]['email'];
+                    $_SESSION['number']=$user[0]['number'];
+                    $_SESSION['group']=$group[0]['name'];
+                    // $_SESSION['subscription_date']=$user[0]['subscription_date'];
+                    // $_SESSION['last_login']=$last_login;
+                    header("Location: ../views/Home/index.php");
+                    exit();
+            }
+                
+            }
+        } else {
+            header("Location: ../");
+            exit();
+        }
+    }
+}catch(Exception $e){
+    $exc=$e->getMessage();
+      $date = date('d.m.Y h:i:s');
+      $log = $exc."   |  Date:  ".$date."\n";
+      echo 'error';
+    //   error_log("$log", 3, "../assets/log-files/log.log");
+}
---
+++ views/Home/index.php
@@ -0,0 +1,103 @@
+
+<?php 
+include "./../adminHeader.php";
+            include "../../sidebar.php";
+                   ?>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link rel="stylesheet" href="./assets/css/style.css"/>
+        <!DOCTYPE html>
+<html>
+<head>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+  <title>Admin</title>
+</head>
+<body >
+
+
+
+<!-- <div class="container-fluid " style="display: flex;
+  justify-content: center;
+  align-items: center; margin-left:-200px">
+  <div class="d-flex flex-column">
+    <div class="col-md-6">
+      <div id="pieChart" style="height: 360px; width: 100%;">
+      </div>
+    </div> -->
+
+  </body>
+
+</html>
+     <?php
+if(isset($_GET["group"])&&!isset($_GET["delete"])){
+    if($_GET["group"]==intval($_GET["group"])){
+    
+             require_once("views/Groups/Groupedit.php");
+         }
+         elseif(($_GET["group"])=='delete'){
+             require_once("views/Groups/Groupdelete.php"); 
+         }
+         else{
+             require_once("views/Groups/GroupsView.php"); 
+         }
+     }
+
+     elseif(isset($_GET["user"])&&!isset($_GET["delete"])){
+        if($_GET["user"]==intval($_GET["user"])) {
+            require_once("views/Users/UsersEdit.php");
+        }
+        else if(($_GET["user"])=='delete'){
+            require_once("views/Users/UsersDelete.php");
+        }
+        else if(($_GET["user"])=='add'){
+            require_once("views/Users/UsersCreate.php");
+        }
+        else{
+            require_once("views/Users/UsersView.php"); 
+        }
+    }
+
+    elseif(isset($_GET["article"])&&!isset($_GET["delete"])){
+        if($_GET["article"]==intval($_GET["article"])){
+        
+                 require_once("views/Articles/ArticlesEdit.php");
+             }
+             elseif(($_GET["article"])=='delete'){
+                 require_once("views/Articles/ArticlesDelete.php"); 
+             }
+               else if(($_GET["article"])=='add'){
+                require_once("views/Articles/ArticlesCreate.php");
+            }
+             else{
+                 require_once("views/Articles/ArticlesView.php"); 
+             }
+         }
+    elseif(isset($_GET["login"])) {
+        require_once("views/Login/Login.php");
+    }
+    else{
+        echo "<body >";
+        echo "<d class='container-fluid' style='display: flex;  justify-content: center;  margin-rigt:200px;'";
+        echo "<div class='d-flex flex-column'>";
+        echo "<div class='col-md-6'>";
+        echo "<div id='pieChart' style='height: 360px; width: 100%;'>";
+        echo "</div>";
+        echo "</div>";
+
+    }
+?> 
+  
+   
+    <script type="text/javascript" src="./assets/js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</script>
+
+
+</body>
+ 
+</html>
