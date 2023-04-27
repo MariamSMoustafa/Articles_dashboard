2dc732ba205354d719bc2c0c72c5463a57c23aed
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-04-28 01:41:24 +0200

    edit in new index

--- views/Home/index.php
+++ views/Home/index.php
@@ -6,7 +6,7 @@ include "../../adminHeader.php";
             <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-       <link rel="stylesheet" href="./assets/css/style.css"/>
+       <link rel="stylesheet" href="../../assets/css/style.css"/>
         <!DOCTYPE html>
 <html>
 <head>
@@ -74,9 +74,9 @@ if(isset($_GET["group"])&&!isset($_GET["delete"])){
                  require_once("views/Articles/ArticlesView.php"); 
              }
          }
-    elseif(isset($_GET["login"])) {
-        require_once("views/Login/Login.php");
-    }
+    // elseif(isset($_GET["login"])) {
+    //     require_once("views/Login/Login.php");
+    // }
     else{
         echo "<body >";
         echo "<d class='container-fluid' style='display: flex;  justify-content: center;  margin-rigt:200px;'";
@@ -90,7 +90,7 @@ if(isset($_GET["group"])&&!isset($_GET["delete"])){
 ?> 
   
    
-    <script type="text/javascript" src="./assets/js/script.js"></script>
+    <script type="text/javascript" src="../../assets/js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
