fe4f42611c99d219b2466785a30dcad1498fb0f2
Mariam <mariansaad585@gmail.com>
2023-04-28 23:30:36 +0300

    user controllers and some style edits

---
+++ controllers/UserController.php
@@ -0,0 +1,43 @@
+<?php 
+function store_user(){            
+            // include "../../Database/MySQLHandler.php";
+            $handler = new MySQLHandler("users");
+
+            if(isset($_POST['submit'])){
+             $newdata=array("id"=>null,"name"=>$_POST['name'] , "email"=>$_POST['email'] , "group_id"=>$_POST['group_id'],"number"=>$_POST['number'],"password"=>$_POST['password']);
+             $handler->connect();
+             $handler->save($newdata);
+             header("Location: http://localhost/Articles_dashboard/views/Home/index.php?user");
+            }
+}
+
+function delete_user(){
+    
+ $handler = new MySQLHandler("users");
+ 
+ $id=intval($_GET['id']);
+ $res=$handler->get_record_by_id($id);
+ if(isset($_POST['submit'])){
+  $handler->connect();
+  $handler->delete($id);
+  header("Location: http://localhost/Articles_dashboard/views/Home/index.php?user");
+}
+}
+
+function edit_user(){    
+ $handler = new MySQLHandler("users");
+ $id=intval($_GET['user']);
+ $res=$handler->get_record_by_id($id);
+ return $res;
+}
+function update_user(){
+    $handler = new MySQLHandler("users");
+    $id=intval($_GET['user']);
+    if(isset($_POST['update'])){
+        $newdata=array("name"=>$_POST['name'] ,"number"=>$_POST['number'],"group_id"=>$_POST['group_id']);
+          $handler->connect();
+      $handler->update($newdata,$id);
+      header("Location: http://localhost/Articles_dashboard/views/Home/index.php?user");
+    
+    }
+}
