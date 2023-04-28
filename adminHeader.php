9082e23628dbb788af602718f81b15132c12c156
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-04-28 14:04:18 +0200

    edits in image

--- adminHeader.php
+++ adminHeader.php
@@ -8,7 +8,7 @@
  <nav  class="navbar navbar-expand-lg navbar-light px-5" style="background-color: #3B3131;">
     
     <a class="navbar-brand ml-5" href="./index.php">
-        <img src="./assets/images/logo.png" width="80" height="80" alt="Swiss Collection">
+        <img src="../../assets/images/logo.png" width="80" height="80" alt="Swiss Collection">
     </a>
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
     
