ba43bf5bff6d32c684e1b5cf278338d43e8a092c
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-04-29 00:08:52 +0200

    edits in permissions

--- views/Users/UsersView.php
+++ views/Users/UsersView.php
@@ -30,7 +30,9 @@ $result=$handler->search('group_id', $groupid);
             echo "<th>group id</th>";
         
 
+if($_SESSION['group']=='Admins') {
     echo "<th>Action</th></thead></tr>";
+}
     
 
     foreach($result as $row)
@@ -47,15 +49,16 @@ $result=$handler->search('group_id', $groupid);
 
         echo "<td>" . $row['group_id'] . "</td>";
 
-
-        echo  
-         "<td>
+if($_SESSION['group']=='Admins') {
+    echo
+     "<td>
          <a  style='color:#584e46; font-weight:bold' href='". $_SERVER["PHP_SELF"]. "?user=". $row["id"]." '> Edit </a><br>
          <a  style='color:#584e46; font-weight:bold' href=" . $_SERVER["PHP_SELF"] ."?user=delete&" ."id=".  $row["id"]  ." name='delete' type='submit'
          
          > Delete </a>  
         
          </td> ";
+}
 
         echo "</tr>";
 
@@ -65,10 +68,9 @@ $result=$handler->search('group_id', $groupid);
     ?>
 
     <?php
-        echo
- 
-        "<a style='color:white; font-weight:bold; Background-color:#584e46' href=" . $_SERVER["PHP_SELF"] ."?user=add&" ."id=" . $row["id"] ." name='add' type='button' class='btn btn-secondary'>Add User</a>"
-
+    if($_SESSION['group']=='Admins')
+        echo"<a style='color:white; font-weight:bold; Background-color:#584e46' href=" . $_SERVER["PHP_SELF"] ."?user=add&" ."id=" . $row["id"] ." name='add' type='button' class='btn btn-secondary'>Add User</a>"
+    
     ?>
    
     
