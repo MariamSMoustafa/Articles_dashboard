1fa22b02a154c2ebd325c97542bfb2e2f82a30ee
MariamBakry <122481396+MariamBakry@users.noreply.github.com>
2023-04-29 13:33:09 +0300

    edits

--- views/Groups/Groupedit.php
+++ views/Groups/Groupedit.php
@@ -1,32 +1,14 @@
-<?php 
- 
-$handler = new MySQLHandler("groups");
-
- $id=intval($_GET['group']);
- $res=$handler->get_record_by_id($id);
- if(isset($_POST['submit'])){
-  move_uploaded_file($_FILES['icon']['tmp_name'], "./assets/images/". $_FILES['icon']['name']);
-  $newdata=array("name"=>$_POST['name'] , "icon"=>"./assets/images/".$_FILES["icon"]['name'] ,"description"=>$_POST['desc']);
-  $handler->connect();
-  $handler->update($newdata,$id);
-  header("Location: http://localhost/Articles_dashboard/views/Home/index.php?group");
- }
-
-
- 
-                   ?>
-           
 <div class="container p-5">
 
 <h4 style="color:#584e46">Edit Group Detail</h4>
-
-<form id="update-Items" enctype='multipart/form-data' action="" method="POST">
+<?php $res=edit_group() ?>
+<form id="update-Items" enctype='multipart/form-data' action="<?php echo update_group()?>" method="POST">
 	<div class="form-group">
       <input type="text" class="form-control" id="product_id"  hidden>
     </div>
     <div class="form-group">
       <label for="name">Group Name:</label>
-      <input name="name" type="text" class="form-control" id="g_name" value="<?=$res[0]['name']?>">
+      <input name="name" type="text" class="form-control" id="g_name" value="<?=$res[0]['name']?>" alt="no image">
     </div>
     <div class="form-group">
       <label for="desc">Group Description:</label>
@@ -38,7 +20,7 @@ $handler = new MySQLHandler("groups");
       <image name="icon" class="mt-2" src="<?=$res[0]['icon']?>" style="width: 50px;">
     </div>
    
-    <button style="background-color:#584e46; color:white; border:none" name="submit" type="submit" class="btn btn-primary">Update</button>
+    <button style="background-color:#584e46; color:white; border:none"  name="submit" type="submit" class="btn btn-primary">Update</button>
   </form>
 
     </div>
