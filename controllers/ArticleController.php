23982a0c83c4396a1219d3c5759216bf2426052f
Mariam <mariansaad585@gmail.com>
2023-05-02 23:23:32 +0300

    debugging

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -1,32 +1,32 @@
 <?php 
-function store_article(){            
-try{
-    $handler = new MySQLHandler("articles");
-    $userhandler = new MySQLHandler("users");
-    $user_name=$userhandler->search('name', $_SESSION['name']);
-    $user_id=$user_name[0]["id"];
+function store_article(){
+    try {
+        $handler = new MySQLHandler("articles");
+        $userhandler = new MySQLHandler("users");
+        $user_name=$userhandler->search('name', $_SESSION['name']);
+        $user_id=$user_name[0]["id"];
 
-  if(isset($_POST['submit'])){
-      $file_type = $_FILES['image']['type'];
-      if ($file_type == 'image/png' || $file_type == 'image/jpeg' ) {
-          $newdata=array("id"=>null,"title"=>$_POST['title'] ,"image"=>"../../assets/images/".$_FILES["image"]['name'], "summery"=>$_POST['summery'] ,"user_id"=>$user_id,"full-article"=>$_POST['full-article']);
-          $handler->connect();
-          $handler->save($newdata);
-          header("Location: http://localhost/Articles_dashboard/views/Home/index.php?article");
-      }else{ 
-        header("Location: http://localhost/Articles_dashboard/views/Home/index.php?article=add&error=image type not supported, must be image/png or image/jpeg");     
-        throw new Exception("article create failed because of image wrong format");
-       }
+        if(isset($_POST['submit'])) {
+            $file_type = $_FILES['image']['type'];
+            if ($file_type == 'image/png' || $file_type == 'image/jpeg') {
+                $newdata=array("id"=>null,"title"=>$_POST['title'] ,"image"=>"../../assets/images/".$_FILES["image"]['name'], "summery"=>$_POST['summery'] ,"user_id"=>$user_id,"full-article"=>$_POST['full-article']);
+                $handler->connect();
+                $handler->save($newdata);
+                header("Location: http://localhost/Articles_dashboard/views/Home/index.php?article");
+            } else {
+                header("Location: http://localhost/Articles_dashboard/views/Home/index.php?article=add&error=image type not supported, must be image/png or image/jpeg");
+                throw new Exception("article create failed because of image wrong format");
+            }
+        }
+    } catch(Exception $e) {
+        $exc=$e->getMessage();
+        $date = date('d.m.Y h:i:s');
+        $log = $exc."   |  Date:  ".$date."\n";
+        echo 'error';
+        error_log("$log", 3, "../../assets/log-files/log.log");
+    }
+
 }
-}catch(Exception $e){
-  $exc=$e->getMessage();
-    $date = date('d.m.Y h:i:s');
-    $log = $exc."   |  Date:  ".$date."\n";
-    echo 'error';
-    error_log("$log", 3, "../../assets/log-files/log.log");
-}
-
-
 function show_articles(){
     $handler = new MySQLHandler("articles");
     if(isset($_GET['id'])){
