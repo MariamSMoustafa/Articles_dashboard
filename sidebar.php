be2fb4c51bd5048ea55d7ae7877cdde98665261e
Esraamohamed0 <esraaelsyed320@gmail.com>
2023-05-05 16:42:44 +0200

    user name in side bar

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 <div class="sidebar" id="mySidebar">
 <div class="side-header">
     <img src="../../assets/images/logo.png" width="120" height="120" alt="Swiss Collection"> 
-    <h5 style="margin-top:10px;">Hello, Admin</h5>
+    <h5 style="margin-top:10px;">Hello,<?php echo  $_SESSION['name'];?></h5>
 </div>
 
 <hr style="border:1px solid; background-color:#8a7b6d; border-color:#3B3131;">
